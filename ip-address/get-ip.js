7ee2d320ee9f6f86474cf5204bb149ae3a8c1ce6
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-10-07 17:18:51 -0300

    updated node.js course [realtime chat with socket.io]

--- ip-address/get-ip.js
+++ ip-address/get-ip.js
@@ -2,3 +2,10 @@ const address = require('address')
 
 const ip = address.ip()
 console.log(`Su dirección IP es: ${ip}`)
+
+const add = address.address((a) => {
+  const ip = a.message
+  console.log(ip)
+})
+
+console.log(add)
